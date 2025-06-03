96d0b5253eab0fc865c80fa774280b66f33b9097
adil <5231841+adil-soubki@users.noreply.github.com>
2025-06-03 12:08:22 +0100

    add benchmarking to workflow

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -5,3 +5,11 @@ SUITE = BenchmarkGroup()
 SUITE["rand"] = @benchmarkable rand(10)
 
 # Write your benchmarks here.
+include("../example.jl")
+SUITE["example"] = @benchmarkable main()
+
+# Run the suite.
+tune!(SUITE)
+results = run(SUITE, verbose=true)
+
+BenchmarkTools.save("output.json", median(results))
